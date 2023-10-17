8e1977ad4da81033d0b620753dc664e5f182bdfb
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-10-17 09:02:28 +0700

    Add example env

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -20,7 +20,7 @@ export class MailService {
                 to, // list of receivers
                 from: process.env.SENDER_EMAIL,
                 subject: 'Password reset for SmartResident account', // Subject line
-                text: `Please use the following link to reset your password: ${process.env.CLIENT_URL}/reset-password?token=${resetToken}\n\nPlease ignore this email if you did not request a password reset.\n\n\nSmartResident Team`, // plaintext body
+                text: `Please use the following link to reset your password: ${process.env.FRONT_END_URL}/reset-password?token=${resetToken}\n\nPlease ignore this email if you did not request a password reset.\n\n\nSmartResident Team`, // plaintext body
             })
             .then(() => {
                 console.log('Email sent successfully');
